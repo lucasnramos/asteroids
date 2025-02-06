457e48d3b0491b6b4336fe25b4dec3bd9f5469b2
Lucas Ramos <lucasnramos@gmail.com>
2025-02-06 10:31:48 -0300

    player moving

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
 import pygame
 from player import Player
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
-    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
