70ba8fb638d61b38b6fa3768caad34030eb5dc9b
Lucas Ramos <lucasnramos@gmail.com>
2025-02-18 07:25:32 -0300

    Readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple asteroids games with pygame
+
+pretty much followed this course from [boot.dev](https://www.boot.dev/courses/build-asteroids-python)
--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
+import random
 
 
 class Asteroid(CircleShape):
@@ -12,3 +14,20 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            self.split_into_new()
+
+    def split_into_new(self):
+        random_angle = random.uniform(20, 50)
+        velocity_a = self.velocity.rotate(random_angle)
+        velocity_b = self.velocity.rotate(-random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid_a = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_a.velocity = velocity_a * 1.2
+        asteroid_b = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_b.velocity = velocity_b * 1.2
